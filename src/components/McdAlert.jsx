ccb799d3303b4dc99fbc0c59492537301fe63a4e
Michael Elliot <michael@digital.com.au>
2019-11-26 18:14:08 +0800

    Fix McdAlert launch date

--- src/components/McdAlert.jsx
+++ src/components/McdAlert.jsx
@@ -18,7 +18,7 @@ class McdAlert extends React.Component {
       <InlineNotification
         class="mcd-alert"
         caption="多抵押 Dai 和 Oasis"
-        message="多抵押 Dai 已于 2019 年 11 月 19 日上线，你可以在 Oasis.app 开启金库抵押多种资产借 Dai，存 Dai 生息并且进行交易。当前面板只能生成单抵押 Sai。"
+        message="多抵押 Dai 已于 2019 年 11 月 18 日上线，你可以在 Oasis.app 开启金库抵押多种资产借 Dai，存 Dai 生息并且进行交易。当前面板只能生成单抵押 Sai。"
         buttonText="前往 Oasis"
         onCloseButtonClick={ () => { localStorage.setItem('McdAlertClosed', true); this.setState({show: false}); } }
         onButtonClick={ () => window.open("https://oasis.app", "_blank") }
