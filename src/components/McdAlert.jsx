d5293b11d0eafac2c581dba39eb7a6d9f67275f7
Lawrence Wang <l@lw.io>
2020-05-22 11:25:52 -0700

    disable functionality
    just show migration banner and image from first slide

--- src/components/McdAlert.jsx
+++ src/components/McdAlert.jsx
@@ -18,11 +18,16 @@ class McdAlert extends React.Component {
       <InlineNotification
         class="mcd-alert"
         caption="多抵押 Dai 和 Oasis"
-        message="单抵押 Dai (Sai) 已于 2020 年 5 月 12 日 UTC 时间 16 时正式关闭。现在开始，Sai 持有者和 CDP 持有者只能通过 MakerDAO 官方窗口 https://migrate.makerdao.com 赎回抵押资产。了解更多信息，请访问我们的论坛 https://forum.makerdao.com/ 或聊天室 https://chat.makerdao.com/home。"
         buttonText="前往赎回窗口"
         onCloseButtonClick={ () => { localStorage.setItem('ScdAlertClosed', true); this.setState({show: false}); } }
         onButtonClick={ () => window.open("https://migrate.makerdao.com", "_blank") }
-      />
+      >
+        单抵押 Dai (Sai) 已于 2020 年 5 月 12 日 UTC 时间 16 时正式关闭。
+        现在开始，Sai 持有者和 CDP 持有者只能通过 MakerDAO 
+        官方窗口 <a href="https://migrate.makerdao.com">https://migrate.makerdao.com</a> 赎回抵押资产。
+        了解更多信息，请访问我们的论坛 <a href="https://forum.makerdao.com/">https://forum.makerdao.com</a> 
+        或聊天室 <a href="https://chat.makerdao.com/">https://chat.makerdao.com</a>。
+      </InlineNotification>
     )
   }
 }
